cc52997909f882cc5f326413e5375c10670fa495
rschaell <d.schaellert@gmail.com>
2024-12-10 19:53:22 +0100

    2nd Run

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,21 +1,18 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
  
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.example.demo.model.SortRequest;
 import com.example.demo.service.BubbleSortService;
 import com.example.demo.service.MergeSortService;
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,15 +12,20 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<String> handleInvalidFormatException(InvalidFormatException ex) {
+        String errorMessage = "Invalid input: Please ensure all elements in the list are numeric.";
+        logger.error(errorMessage, ex);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body("Invalid input: Please ensure all elements in the list are numeric.");
+                .body(errorMessage);
     }
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        logger.error(ex.getReason(), ex);
         return ResponseEntity
                 .status(ex.getStatusCode())
                 .body(ex.getReason());
