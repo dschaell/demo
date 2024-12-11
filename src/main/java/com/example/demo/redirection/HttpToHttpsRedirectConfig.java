c09b44fc725333f71143e89ae51f82442766aaca
rschaell <d.schaellert@gmail.com>
2024-12-11 09:38:15 +0100

    HTTPS eingebaut

---
+++ src/main/java/com/example/demo/redirection/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.redirection;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpToHttpsRedirectConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8100); // HTTP-Port
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // HTTPS-Port
+        return connector;
+    }
+}
